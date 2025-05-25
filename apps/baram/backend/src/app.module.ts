fb316ab1a35c61b880f9f54e3b09d0a0c66c8222
Grooveturtle29 <grooveturtle29@gmail.com>
2025-05-25 23:51:07 +0900

    test get api

--- apps/baram/backend/src/app.module.ts
+++ apps/baram/backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
+import { TestModule } from "./test/test.module";
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from "@nestjs/config";
       isGlobal: true,
       envFilePath: ".env",
     }),
+    TestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/baram/backend/src/main.ts
+++ apps/baram/backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // FIXME: 개발중 임시 허용
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
