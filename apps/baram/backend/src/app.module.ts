70d9fce2a5072aa171948489ea15fecc6650f1b9
Grooveturtle29 <grooveturtle29@gmail.com>
2025-05-26 23:49:03 +0900

    typeorm mysql 설정 추가

--- apps/baram/backend/src/app.module.ts
+++ apps/baram/backend/src/app.module.ts
@@ -3,12 +3,24 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { TestModule } from "./test/test.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
+    }),
+    TypeOrmModule.forRoot({
+      //데이터베이스 타입
+      type: "mysql",
+      host: process.env["DB_HOST"],
+      port: parseInt(process.env["DB_PORT"] as string),
+      username: process.env["DB_USERNAME"],
+      password: process.env["DB_PASSWORD"],
+      database: process.env["DB_DATABASE"],
+      entities: [],
+      synchronize: true,
     }),
     TestModule,
   ],
