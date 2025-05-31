8de4bdaf9c36715101044ae431dc38c82c56b170
grooveTurtle <grooveturtle29@gmail.com>
2025-05-31 22:08:46 +0900

    apps admin nestjs project add

---
+++ apps/admin/backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      //데이터베이스 타입
+      type: 'mysql',
+      host: process.env['DB_HOST'],
+      port: parseInt(process.env['DB_PORT'] as string),
+      username: process.env['DB_USERNAME'],
+      password: process.env['DB_PASSWORD'],
+      database: process.env['DB_DATABASE'],
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
