a60ef7cacffbfd1c18db3d6c7ecaf91c98c7d61a
grooveTurtle <grooveturtle29@gmail.com>
2025-06-01 23:52:01 +0900

    boardpost entity,dto -> base_post 로 이전

--- packages/core/src/base-board/dto/create-board-post.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PickType } from '@nestjs/mapped-types';
-import { BoardPost } from '../entity/board-post';
-
-export class CreateBoardPostDto extends PickType(BoardPost, [
-  'guest_author_id',
-  'guest_author_password',
-  'title',
-  'content',
-]) {}
