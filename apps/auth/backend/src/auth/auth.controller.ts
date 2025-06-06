a78799054fc4753b67a121f4990b88c71182d74f
grooveTurtle <grooveturtle29@gmail.com>
2025-06-07 00:41:37 +0900

    BasicUserTokenGuard 적용 해보기

--- apps/auth/backend/src/auth/auth.controller.ts
+++ apps/auth/backend/src/auth/auth.controller.ts
@@ -10,7 +10,7 @@ import {
 import { AuthService } from './auth.service';
 import { CreateUserAccountDto } from '@_core/base-user/dto/create-user-account.dto';
 import { BaseAuthService } from '@_core/base-auth/base-auth.service';
-import { BasicTokenGuard } from '@_core/base-auth/guard/basic-token.guard';
+import { BasicUserTokenGuard } from '@_core/base-auth/guard/basic-user-token.guard';
 import { Request as ExpressRequest } from 'express';
 import { UserAccount } from '@_core/base-user/entity/user-account.entity';
 
@@ -31,11 +31,8 @@ export class AuthController {
   }
 
   @Post('login')
-  @UseGuards(BasicTokenGuard)
+  @UseGuards(BasicUserTokenGuard)
   async loginUser(@Request() req: BoardPostRequest) {
     return this.authService.loginUser(req.loginInfo);
   }
-
-  @Post('test')
-  testAccess() {}
 }
