3e20f112b59a2427a3456dc4894be67ee51d7f15
grooveTurtle <grooveturtle29@gmail.com>
2025-06-08 23:54:56 +0900

    resource-info - class-validator 추가

--- packages/core/src/base-common/entity/resource-info.embeddable.ts
+++ packages/core/src/base-common/entity/resource-info.embeddable.ts
@@ -1,17 +1,21 @@
 import { Column } from "typeorm";
 import { ResourceType } from "@_core/base-common/enum/common.enum";
+import { IsEnum, IsNumber } from "class-validator";
 
 /**
  * 같은 기능에 resource가 구분되는 경우에 사용되는 공용 embeddable 클래스
  * 댓글, 좋아요 (게시판, 댓글 등)
  */
 export class ResourceInfo {
+  @IsEnum(ResourceType)
   @Column({ type: "enum", enum: ResourceType })
   resource_type: ResourceType;
 
+  @IsNumber()
   @Column()
   resource_id: number;
 
+  @IsNumber()
   @Column({ nullable: true })
   resource_sub_id?: number;
 }
