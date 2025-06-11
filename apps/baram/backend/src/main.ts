875981d287331db1f038dfb5536167e6acb8a0ca
grooveTurtle <grooveturtle29@gmail.com>
2025-06-12 01:28:28 +0900

    baram app transfrom 적용

--- apps/baram/backend/src/main.ts
+++ apps/baram/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,17 @@ async function bootstrap() {
   app.enableCors({
     origin: ["http://localhost:3000"],
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
 
   await app.listen(process.env.PORT ?? 4000);
 }
