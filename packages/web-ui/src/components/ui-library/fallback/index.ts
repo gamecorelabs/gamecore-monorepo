a4e4a5ac0e4a0d7057821fc583f2878f3d2a80b8
grooveTurtle <grooveturtle29@gmail.com>
2025-06-18 20:00:47 +0900

    프론트 공통 컴포넌트 분리

---
+++ packages/web-ui/src/components/ui-library/fallback/index.ts
@@ -0,0 +1,1 @@
+export { default as FallbackPage } from "./FallbackPage";
--- packages/web-ui/src/components/ui-library/index.ts
+++ packages/web-ui/src/components/ui-library/index.ts
@@ -1,3 +1,4 @@
 export * from "./layouts";
 export * from "./board";
 export * from "./floating";
+export * from "./fallback";
