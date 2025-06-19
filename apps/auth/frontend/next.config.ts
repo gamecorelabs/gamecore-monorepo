f311a981c265059f55e77648c2aca3c731376747
grooveTurtle <grooveturtle29@gmail.com>
2025-06-19 21:55:07 +0900

    auth frontend next 서버 셋팅

---
+++ apps/auth/frontend/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  transpilePackages: ["@gamecoregg/web-ui"],
+};
+
+export default nextConfig;
