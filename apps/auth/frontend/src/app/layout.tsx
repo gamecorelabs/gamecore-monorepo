ccf4e8c81473fdfadac8f20e3e26d2c151160cd8
grooveTurtle <grooveturtle29@gmail.com>
2025-06-20 19:56:21 +0900

    auth frontend login register page 추가

--- apps/auth/frontend/src/app/layout.tsx
+++ apps/auth/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Container from "@gamecoregg/web-ui/components/ui-library/layouts/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({
     <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ></body>
+      >
+        <Container>{children}</Container>
+      </body>
     </html>
   );
 }
