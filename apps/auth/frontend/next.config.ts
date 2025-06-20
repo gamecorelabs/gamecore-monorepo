5e754f51d264987c99994c6fa537232aa95af36d
grooveTurtle <grooveturtle29@gmail.com>
2025-06-20 23:47:24 +0900

    auth frontend 삭제 - 프론트까지 별도로 분리하지는 않기로

--- apps/auth/frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  transpilePackages: ["@gamecoregg/web-ui"],
-};
-
-export default nextConfig;
