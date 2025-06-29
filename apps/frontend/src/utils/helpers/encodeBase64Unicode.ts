49f36d4cee7f8a0746be794967cbc93b391868f2
grooveTurtle <grooveturtle29@gmail.com>
2025-06-29 19:00:47 +0900

    비회원 아이디 자음 허용으로 변경

--- apps/frontend/src/utils/helpers/encodeBase64Unicode.ts
+++ apps/frontend/src/utils/helpers/encodeBase64Unicode.ts
@@ -1,3 +1,6 @@
 export function encodeBase64Unicode(str: string): string {
-  return btoa(decodeURIComponent(encodeURIComponent(str)));
+  const utf8Bytes = new TextEncoder().encode(str);
+  let binary = "";
+  utf8Bytes.forEach((byte) => (binary += String.fromCharCode(byte)));
+  return btoa(binary);
 }
