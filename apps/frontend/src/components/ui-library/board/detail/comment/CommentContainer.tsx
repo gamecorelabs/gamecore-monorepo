24154e3d1792111690e0311b72cab2df12b03842
grooveTurtle <grooveturtle29@gmail.com>
2025-06-30 17:19:47 +0900

    comment Component 분리

--- apps/frontend/src/components/ui-library/board/detail/comment/CommentContainer.tsx
+++
@@ -1,9 +0,0 @@
-const CommentContainer = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
-  return <div className="mt-6">{children}</div>;
-};
-
-export default CommentContainer;
