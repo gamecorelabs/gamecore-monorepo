c7373983781fc8b30ddd404ec17b19f18bd2ce6e
grooveTurtle <grooveturtle29@gmail.com>
2025-06-30 17:21:12 +0900

    비회원, 회원 닉네임 구분 출력 getUserName helper 추가

---
+++ apps/frontend/src/utils/helpers/getUserName.ts
@@ -0,0 +1,8 @@
+import { GuestAccount, UserAccount } from "@gamecoregg/types/user/user.types";
+
+export const getUserName = (user: {
+  guest_account?: GuestAccount;
+  author?: UserAccount;
+}) => {
+  return user.guest_account?.guest_author_id || user.author?.nickname || "익명";
+};
