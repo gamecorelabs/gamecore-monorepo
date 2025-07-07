550cdce8caf75bb60b2cca79e1a1fb9d13a7e419
grooveTurtle <grooveturtle29@gmail.com>
2025-07-07 15:31:29 +0900

    프론트 get list queryString 조립 추가

--- apps/frontend/src/app/board/[boardId]/post/page.tsx
+++ apps/frontend/src/app/board/[boardId]/post/page.tsx
@@ -1,16 +1,29 @@
 import { ArticleList } from "@ui-library";
 import React from "react";
 import dataApi from "@/utils/common-axios/dataApi";
+import qs from "qs";
+
 interface BoardPostProps {
   params: { boardId: string };
+  searchParams: { [key: string]: string | string[] };
 }
 
-const BoardPostPage = async ({ params }: BoardPostProps) => {
+const BoardPostPage = async ({ params, searchParams }: BoardPostProps) => {
   let posts = null;
 
   const { boardId } = await params;
+  const qstring = await searchParams;
+
+  let queryString = qs.stringify(qstring, {
+    arrayFormat: "brackets",
+  });
+
+  if (queryString) {
+    queryString = `?${queryString}`;
+  }
+
   try {
-    const response = await dataApi.get(`/board/${boardId}/post`);
+    const response = await dataApi.get(`/board/${boardId}/post${queryString}`);
     posts = response?.data ?? [];
   } catch (error) {
     posts = [];
