6cb950f08e1ef3f36845d711c6926ef298bbc9e3
grooveTurtle <grooveturtle29@gmail.com>
2025-07-17 02:40:08 +0900

    프론트만 수정해보기

--- apps/frontend/src/components/ui-library/layouts/Footer/Footer.tsx
+++ apps/frontend/src/components/ui-library/layouts/Footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
         <p>
-          &copy; {new Date().getFullYear()} gamecore.co.kr All rights reserved..
+          &copy; {new Date().getFullYear()} gamecore.co.kr All rights reserved.
         </p>
         <p>
           <a href="#" className="text-gray-400 hover:text-white">
