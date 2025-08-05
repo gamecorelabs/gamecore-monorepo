0c025f526d54529dc435e68b2b08498939773cc1
grooveTurtle <grooveturtle29@gmail.com>
2025-08-05 23:17:26 +0900

    feature: app/not-found.tsx fallback 공통처리

--- apps/frontend/src/app/not-found.tsx
+++ apps/frontend/src/app/not-found.tsx
@@ -1,11 +1,11 @@
 import { FallbackPage } from "@ui-library";
 
-export default function NewsFallbackPage() {
+export default function NotFound() {
   return (
     <FallbackPage
       message="잘못된 경로입니다. 메인 페이지로 이동합니다."
       redirectTo="/"
-      delay={2000}
+      delay={3000}
     />
   );
 }
--- apps/frontend/src/app/user/auth/login/page.tsx
+++ apps/frontend/src/app/user/auth/login/page.tsx
@@ -1,20 +1,36 @@
 "use client";
 import { GalleryVerticalEnd } from "lucide-react";
 import { LoginForm } from "@ui-library/auth/LoginForm";
+import { useUserStore } from "@/store/userStore";
+import FallbackPage from "@/components/ui-library/fallback/FallbackPage";
 
 const LoginPage = () => {
+  const currentUser = useUserStore((state) => state.user);
+
+  if (currentUser) {
+    return (
+      <FallbackPage
+        message="이미 로그인되어 있습니다. 다른 계정으로 로그인하시려면 로그아웃 후 다시 시도해주세요."
+        redirectTo="/"
+        delay={3000}
+      />
+    );
+  }
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <div className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
+    !currentUser && (
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <div className="flex items-center gap-2 self-center font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            GameCore.gg
           </div>
-          GameCore.gg
+          <LoginForm />
         </div>
-        <LoginForm />
       </div>
-    </div>
+    )
   );
 };
 
